ebf53a67eee7fcdc1a80206e51e2e6daa2f6d0d8
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-09-11 00:20:19 -0300

    show my ip hotfix

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.enable('trust proxy');
+
 app.get('/', function (req, res) {
-  res.send('We believe your ip is: ' + req.ip); //.toString()?
+  res.send('We believe your ip is: ' + req.ip);
 });
 
 app.listen(3000, function () {
