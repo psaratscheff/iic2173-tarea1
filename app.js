d359770e927611865bec257765009c7a236d32cb
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-09-11 01:14:27 -0300

    show 3 ips

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
       db.close();
   });
 
-  res.send('We believe your ip is: ' + req.ip + '\n and the last ip was: ' + ip_list);
+  res.send('We believe your ip is: ' + req.ip + '\n and the lastest ips were:\n' + '1: ' + ip_list[0]['ip'] + '\n2: ' + ip_list[1]['ip'] + '\n3: ' + ip_list[2]['ip']);
 });
 
 app.listen(3000, function () {
