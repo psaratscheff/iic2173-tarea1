c67952939b0dae5b16014d85cf0f724740e1c731
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-09-11 01:29:40 -0300

    index list + reverse order (latest first)

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ app.get('/', function (req, res) {
       var d = new Date();
       collection.insert({ ip: req.ip, date: d })
       collection.find().toArray(function(err, results) {
-        ip_list = results;
+        ip_list = results.reverse();
       });
       db.close();
   });
 
   var html = '<html><body>We believe your ip is: ' + req.ip + '<br/>The Latest IPs where:'
+  var i = 1;
   ip_list.forEach(function(ip_data){
-    html = html + '<br/>Date: ' + ip_data['date'] + ' /// IP: ' + ip_data['ip']
+    html = html + '<br/>' + i + '==>  Date: ' + ip_data['date'] + ' /// IP: ' + ip_data['ip'];
+    i++;
   });
   html = html + '</body></html>'
 
